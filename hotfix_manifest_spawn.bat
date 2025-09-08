71e169c271b54651fe9ef9ceaa9ffa703b0ee53f
Joseph <>
2025-09-08 16:31:18 +0800

    docs

--- hotfix_manifest_spawn.bat
+++ hotfix_manifest_spawn.bat
@@ -5,6 +5,6 @@ set /p version="input project version(1.0.0):"
 set remoteUrl=https://clicknpop.github.io/test/
 @REM 本地需要加入熱更的資源路徑
 set assetsPath=.\build\android\assets\
-@REM 新manifest文件產出的位置
+@REM 新manifest產出的位置
 set exportPath=.\export\
 call node version_generator.js -v %version% -u %remoteUrl% -s %assetsPath% -d %exportPath%
